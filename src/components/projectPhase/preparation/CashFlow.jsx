99c68dba7215f1640af7aff65ac7c0c2da652be1
Yash Sain <yashsain.2406@gmail.com>
2024-02-28 10:46:20 +0530

    table updated

--- src/components/projectPhase/preparation/CashFlow.jsx
+++ src/components/projectPhase/preparation/CashFlow.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLanguage } from "../../../context/LanguageContext";
-import "./evaluation.css";
+
 import { useTranslation } from "react-i18next";
 function CashFlow() {
   const { language, toggleLanguage } = useLanguage();
@@ -10,41 +10,42 @@ function CashFlow() {
       <p className="text-xl font-normal mt-6  text-[#808080]">
         {t("loremMed")}
       </p>
-
-      <thead className="mt-8">
-        <tr>
-          <th className="th rounded-tl-md">{t("correlation")}</th>
-          <th className="th">{t("phaseCost")}</th>
-          <th className="th">{t("items%")}</th>
-          <th className="th">{t("phaseDur")}</th>
-          <th className="th rounded-tr-md">{t("%cumulative")}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="bg-white">
-          <td className="td rounded-bl-md">500</td>
-          <td className="td">-</td>
-          <td className="td">-</td>
-          <td className="td">-</td>
-          <td className="td rounded-br-md">-</td>
-        </tr>
-      </tbody>
-
-      <thead className="mt-12">
-        <tr>
-          <th className="th rounded-tl-md">{t("percOfInvoice")}</th>
-          <th className="th">{t("phase")}</th>
-          <th className="th rounded-tr-md">{t("no.")}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="bg-white">
-          <td className="td rounded-bl-md">-</td>
-          <td className="td">2</td>
-          <td className="td rounded-br-md">1</td>
-        </tr>
-      </tbody>
-
+      <table className="mt-8 rounded-lg">
+        <thead className="mt-8">
+          <tr>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("correlation")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("phaseCost")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("items%")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("phaseDur")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("%cumulative")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white">
+            <td className="border border-[#828282] p-2">500</td>
+            <td className="border border-[#828282] p-2">-</td>
+            <td className="border border-[#828282] p-2">-</td>
+            <td className="border border-[#828282] p-2">-</td>
+            <td className="border border-[#828282] p-2">-</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="mt-8 rounded-lg">
+        <thead className="mt-12">
+          <tr>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("percOfInvoice")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("phase")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("no.")}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white">
+            <td className="border border-[#828282] p-2">-</td>
+            <td className="border border-[#828282] p-2">2</td>
+            <td className="border border-[#828282] p-2">1</td>
+          </tr>
+        </tbody>
+      </table>
       <div className="flex flex-row md:gap-10 gap-3 mt-16">
         <button className="bg-primary py-1 px-4 rounded-3xl font-medium    text-white">
           {t("print")}
--- src/components/projectPhase/preparation/Evaluation.jsx
+++ src/components/projectPhase/preparation/Evaluation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./evaluation.css";
+
 import { FormControl, MenuItem, Select } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useLanguage } from "../../../context/LanguageContext";
@@ -55,27 +55,29 @@ function Evaluation() {
         {t("loremMed")}
       </p>
       <div className="flex flex-col items-center mt-8">
+      <table className="mt-2 rounded-lg">
         <thead>
           <tr>
-            <th className="th rounded-tl-md">{t("cumulative")}</th>
-            <th className="th">{t("weight")}</th>
-            <th className="th">{t("description")}</th>
-            <th className="th">{t("criType")}</th>
-            <th className="th rounded-tr-md">{t("#")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("cumulative")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("weight")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("description")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2">{t("criType")}</th>
+            <th className="border border-[#828282] bg-[#dfe6fc] p-2 rounded-tr-md">{t("#")}</th>
           </tr>
         </thead>
         <tbody>
           {evalCri.map((item) => (
             <tr className="bg-white">
-              <td className="td">{item.cumulative}</td>
-              <td className="td">{item.weight} </td>
-              <td className="td">{item.desc} </td>
-              <td className="td">{item.criType}</td>
+              <td className="border border-[#828282] p-2">{item.cumulative}</td>
+              <td className="border border-[#828282] p-2">{item.weight} </td>
+              <td className="border border-[#828282] p-2">{item.desc} </td>
+              <td className="border border-[#828282] p-2">{item.criType}</td>
 
-              <td className="td">{item.some}</td>
+              <td className="border border-[#828282] p-2">{item.some}</td>
             </tr>
           ))}
         </tbody>
+        </table>
         <div className="flex flex-row gap-3 mt-16">
           <button className="bg-primary py-1 px-4 rounded-3xl font-medium    text-white">
             {t("print")}
