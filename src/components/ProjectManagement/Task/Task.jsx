5d593dfd2f79f14f641895de88818cf8a0735407
Sgvreactteam <reactsgv@gmail.com>
2024-03-04 11:37:43 +0530

    new changes today 😎

--- src/components/ProjectManagement/Task/Task.jsx
+++ src/components/ProjectManagement/Task/Task.jsx
@@ -3,64 +3,57 @@ import { useTranslation } from 'react-i18next';
 import TaskList from './TaskList/TaskList';
 import DashBoard from './Dashboard/Dashboard'
 import Timeline from './Timeline/Timeline'
+import Button from '../../UI/Button';
 function Task({checkedA}) {
 
     const { t } = useTranslation();
-    
+    const [taskSelect, setTaskSelect] = useState(0)
 
-    const [dashboard, setDashboard] = useState(true);
-    const [taskList, setTaskList] = useState(false);
-    const [timeline, setTimeline] = useState(false);
-
-    const showDashboard = () => {
-        setDashboard(true);
-        setTaskList(false);
-        setTimeline(false);
+    const handleRegButtonClick = (index) => {
+      setTaskSelect(index)
     }
 
-    const showTasklist = () => {
-        setTaskList(true);
-        setTimeline(false);
-        setDashboard(false);
-    }
 
-    const showTimeline = () => {
-        setTimeline(true);
-        setDashboard(false);
-        setTaskList(false);
-    }
+    const taskRouts =[
+      {
+        title:"dashboard",
+        to:"/task/Dashboard"
+      },
+      {
+        title:"taskList",
+        to:"/task/Tasklist"
+      },
+      {
+        title:"timeline",
+        to:"/task/Timeline"
+      },
+    ]
 
 
   return (
     <div className='bg-[#F2F6FE] h-screen pt-20 whitespace-nowrap'>
         <div className="flex justify-between ml-8 pb-8 mr-8">
         <div className="flex flex-row gap-7 ">
-          <button
-            onClick={showDashboard}
-            className="bg-white py-1 px-4 rounded-3xl font-medium   text-primary focus:bg-primary focus:text-white"
-          >
-              {t('dashboard')}
-          </button>
-          <button
-            onClick={showTasklist}
-            className="bg-white py-1 px-4 rounded-3xl font-medium   text-primary focus:bg-primary focus:text-white"
-          >
-            {t('taskList')}
-          </button>
-          <button
-            onClick={showTimeline}
-            className="bg-white py-1 px-4 rounded-3xl font-medium   text-primary focus:bg-primary focus:text-white"
-          >
-            {t('timeline')}
-          </button>
+        {
+                taskRouts && taskRouts.map((item,index)=>{
+                    return  <Button
+                    type={taskSelect == index ? "regularChange" : "regular"}
+                    to={item.to}
+                    onClick={handleRegButtonClick.bind(this,index)}
+                >
+                    { t(item.title)}
+                </Button>
+                })
+              }
         </div>
-        {!dashboard && <button className='bg-primary text-white py-1 px-4 rounded-2xl'>
+        {/* {!dashboard && <button className='bg-primary text-white py-1 px-4 rounded-2xl'>
             {t('addNewTask')}
-          </button>}
+          </button>} */}
         </div>
-        {taskList && <TaskList/>}
-        {dashboard && <DashBoard/>}
-        {timeline && <Timeline/>}
+                {location.pathname === '/task/Dashboard' && <DashBoard />}
+                {location.pathname === '/task/Tasklist' && <TaskList />}
+                {location.pathname === '/task/Timeline' && <Timeline />}
+              
     </div>
   )
 }
